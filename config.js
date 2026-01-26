e59e7d295a7e2cdb369e8b43580cb1fab3dbf922
gapmfi-ux <gapmfi@gmail.com>
2026-01-26 10:56:00 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
-// Google Apps Script Configuration
+// Google Apps Script Configuration - JSONP Version
 const GAS_CONFIG = {
-    // Replace with your deployed GAS web app URL
-    BASE_URL: 'https://script.google.com/macros/s/AKfycbzmjPEYq5F9FvV9mFPy91ahKtkkIIsnRoPctEZW7yfeQozYM5JjcVVMBfZOI6GX1VXrHQ/exec'
+    // Your deployed GAS web app URL
+    BASE_URL: 'https://script.google.com/macros/s/AKfycbzmjPEYq5F9FvV9mFPy91ahKtkkIIsnRoPctEZW7yfeQozYM5JjcVVMBfZOI6GX1VXrHQ/exec',
+    
+    // JSONP callback name
+    CALLBACK: 'gasCallback'
 };
