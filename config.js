beb4a58397eb9df11203ee44123e5e7d0edbae72
gapmfi-ux <gapmfi@gmail.com>
2026-08-18 10:52:17 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Google Apps Script Configuration
 const GAS_CONFIG = {
     // Your deployed GAS web app URL
-    BASE_URL: 'https://script.google.com/macros/s/AKfycbzmjPEYq5F9FvV9mFPy91ahKtkkIIsnRoPctEZW7yfeQozYM5JjcVVMBfZOI6GX1VXrHQ/exec',
+    BASE_URL: 'https://script.google.com/macros/s/AKfycbz20Xs1Wi2ulnPP_23NWqmoEThjNLRu6JyZeEiXmFMWWWgoOTxCPIpSoQ91idusjNtffw/exec',
     // Enable CORS debugging
     ENABLE_DEBUG: true,
     // Timeout settings (in ms)
